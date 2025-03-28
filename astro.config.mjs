3adbfa263e32e9468239ee9b21c272711ea1030e
artlands <lijie0416@gmail.com>
2025-03-27 23:14:32 -0400

    update template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,34 @@
+// @ts-check
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-
+import tailwindcss from "@tailwindcss/vite";
+import vercel from "@astrojs/vercel";
 import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+import keystatic from "@keystatic/astro";
+import db from "@astrojs/db";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  // The `site` property specifies the base URL for your site.
+  // Be sure to update this to your own domain (e.g., "https://yourdomain.com") before deploying.
   site: "https://nsfcac.github.io/repacss",
   base: "/repacss",
-  output: 'static',
+  prefetch: true,
+  trailingSlash: "never",
+  experimental: {
+    clientPrerender: true,
+  },
+  integrations: [
+    react(),
+    markdoc(),
+    ...(process.env.SKIP_KEYSTATIC ? [] : [keystatic()]),
+    db(),
+    svelte(),
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  output: "server",
+  adapter: vercel(),
 });
