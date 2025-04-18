427d5c06cdabc8230b021042cac7eaa4de697dc6
artlands <lijie0416@gmail.com>
2025-04-17 22:51:16 -0400

    update site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: "https://nsfcac.github.io/repacss",
+  site: "https://repacss.org",
   base: "/repacss",
   output: 'static',
   image: {
