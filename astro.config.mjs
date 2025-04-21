3c1b6daa5718d0e96ee03c4e7517d9a3ee8e455a
artlands <lijie0416@gmail.com>
2025-04-21 17:14:07 -0400

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-// https://astro.build/config
 // https://repacss.org
 // nsfcac.github.io/repacss
 export default defineConfig({
   integrations: [tailwind(), react()],
   site: "https://repacss.org",
-  base: "/repacss",
+  // base: "/repacss",
   output: 'static',
   image: {
     service: { entrypoint: 'astro/assets/services/sharp' }
